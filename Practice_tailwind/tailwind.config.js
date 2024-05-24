dcb98f050b8349abbf79016fea7e81f053ccb8ec
omtamang <119476805+omtamang@users.noreply.github.com>
2024-05-24 17:43:53 +0545

    Add files via upload

---
+++ Practice_tailwind/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'crayola': '#F7F4EC',
+        'back': '#EDE9DD',
+        'footer': '#D0DEEC',
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+
